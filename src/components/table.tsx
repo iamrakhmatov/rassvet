0e287002f543e1715b17d8eb3fba907b7c72c56a
Shakhruz Rakhmatov <iamrakhmatov@gmail.com>
2023-04-23 18:59:48 +0500

    feat: add design for table

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -1,15 +1,16 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React from "react";
 import {
   useReactTable,
   getCoreRowModel,
   flexRender,
   RowData,
   createColumnHelper,
-} from '@tanstack/react-table';
-import { makeData, Products } from './makeData';
+} from "@tanstack/react-table";
+import clsx from "clsx";
+import { makeData, Products } from "./makeData";
 
-declare module '@tanstack/react-table' {
+declare module "@tanstack/react-table" {
   interface TableMeta<TData extends RowData> {
     updateData: (rowIndex: number, columnId: string, value: unknown) => void;
   }
@@ -45,36 +46,49 @@ declare module '@tanstack/react-table' {
 export function EditableTable() {
   const [data, setData] = React.useState(() => makeData(10));
 
+  const inputField = React.useMemo(() => {
+    return <input type="text" className="w-full ring-0 focus:ring-0" />;
+  }, []);
+
   const columns = React.useMemo(() => {
     const columnHelper = createColumnHelper<Products>();
 
     return [
-      columnHelper.accessor('name', {
-        header: 'Name',
+      columnHelper.accessor("name", {
+        header: "Name",
         footer: (props) => props.column.id,
       }),
-      columnHelper.accessor('in_stock', {
-        header: 'In Stock',
+      columnHelper.accessor("in_stock", {
+        header: "Order",
+        cell: (props) => inputField,
+        meta: {
+          header: {
+            width: "20px",
+          },
+        },
+      }),
+      columnHelper.accessor("in_stock", {
+        header: "In Stock",
         footer: (props) => props.column.id,
       }),
-      columnHelper.accessor('booked', {
-        header: 'Booked',
+      columnHelper.accessor("booked", {
+        header: "Booked",
         footer: (props) => props.column.id,
       }),
-      columnHelper.accessor('total', {
-        header: 'Price',
+      columnHelper.accessor("total", {
+        header: "Total",
         footer: (props) => props.column.id,
       }),
-      columnHelper.accessor('price', {
-        header: 'Price',
+      columnHelper.accessor("price", {
+        header: "Price",
         footer: (props) => props.column.id,
       }),
-      columnHelper.accessor('unit', {
-        header: 'Unit',
+      columnHelper.accessor("unit", {
+        header: "Unit",
         footer: (props) => props.column.id,
       }),
     ];
-  }, []);
+  }, [inputField]);
 
   const table = useReactTable({
     data,
@@ -99,15 +113,19 @@ export function EditableTable() {
   });
 
   return (
-    <div className="p-2">
-      <div className="h-2" />
-      <table>
-        <thead>
+    <div className="overflow-hidden ring-1 ring-gray-300  sm:rounded-lg">
+      <table className="min-w-full divide-y divide-gray-300">
+        <thead className="bg-gray-100">
           {table.getHeaderGroups().map((headerGroup) => (
-            <tr key={headerGroup.id}>
+            <tr key={headerGroup.id} className="divide-x divide-gray-200">
               {headerGroup.headers.map((header) => {
                 return (
-                  <th key={header.id} colSpan={header.colSpan}>
+                  <th
+                    key={header.id}
+                    colSpan={header.colSpan}
+                    scope="col"
+                    className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+                  >
                     {header.isPlaceholder
                       ? null
                       : flexRender(
@@ -123,10 +141,23 @@ export function EditableTable() {
         <tbody>
           {table.getRowModel().rows.map((row) => {
             return (
-              <tr key={row.id}>
+              <tr
+                key={row.id}
+                className={clsx(
+                  row.index % 2 === 0 ? "" : "bg-gray-50",
+                  "divide-x divide-gray-200 border-t border-gray-200 "
+                )}
+              >
                 {row.getVisibleCells().map((cell) => {
                   return (
-                    <td key={cell.id}>
+                    <td
+                      key={cell.id}
+                      className={
+                        cell.column.columnDef.header === "Order"
+                          ? "w-24"
+                          : "relative  py-2 pl-4 pr-3 text-sm sm:pl-6"
+                      }
+                    >
                       {flexRender(
                         cell.column.columnDef.cell,
                         cell.getContext()
